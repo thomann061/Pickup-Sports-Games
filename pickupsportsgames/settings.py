f0598416a61c449b83d62da1cca96c3c3a48ec5d
thomann061 <thomann061@gmail.com>
2017-05-05 10:10:43 -0400

    Add templates and static files

--- pickupsportsgames/settings.py
+++ pickupsportsgames/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'webapp',
     'api',
     'rest_framework.authtoken',
     'rest_framework',
@@ -121,6 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
