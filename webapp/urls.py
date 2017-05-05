6c4dfa8f2bb62c3f902b2aeb6f3be1ddf0d0eefa
thomann061 <thomann061@gmail.com>
2017-05-05 10:51:02 -0400

    Add login page

--- webapp/urls.py
+++ webapp/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
  
 urlpatterns = [
-    url(r'^$', views.HomePageView.as_view(), name="home")
+    url(r'^$', views.HomePageView.as_view(), name="home"),
+    url(r'^login$', views.LoginPageView.as_view(), name="login")
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = "index.html"
+
+class LoginPageView(TemplateView):
+    template_name = "login.html"
