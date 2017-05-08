83029b504e86875f018c5516660ced66f81b3390
thomann061 <thomann061@gmail.com>
2017-05-08 13:06:55 -0400

    Update to string

--- api/models.py
+++ api/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 class Game(models.Model):
-    gameName = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     gameType = models.CharField(max_length=255)
     gameLocation = models.CharField(max_length=255)
     gameDateTime = models.DateTimeField()
+
+    # To string
+    def __str__(self):
+        return 'Game #' + str(self.id)
